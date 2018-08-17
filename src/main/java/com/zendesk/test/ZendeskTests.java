28057e010487bf5b8ca789fe93d4aff16337362b
bd2810 <33436672+bd2810@users.noreply.github.com>
2018-08-16 22:46:11 -0400

    more updates

--- src/main/java/com/zendesk/test/ZendeskTests.java
+++ src/main/java/com/zendesk/test/ZendeskTests.java
@@ -7,14 +7,21 @@ import java.io.File;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.zendesk.pageObjects.Tickets;
+
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 public class ZendeskTests {
 
 	private static final String token = "0Jh8yKwpNBJPQjyAEDkB3vWPPjDNe8IUFzisWTOP";
-	public static String ticketID = null;
 
+	/*
+	 * Before class method which sets base path, base URI, and authentication credentials
+	 * which is common to all tests
+	 */
 	@BeforeClass
 	public static void setup() {
 
@@ -23,12 +30,63 @@ public class ZendeskTests {
 		RestAssured.authentication = RestAssured.basic("bhavik.doshi@gmail.com/token", token);
 	}
 
+	/*
+	 * Below test will create a new ticket using a JSON file
+	 */
 	@Test
 	public void createTicket() throws Exception {
 
 		File jsonFile = new File("./src/main/resources/newTicket.json");
 
 		given().contentType("application/json").body(jsonFile).when().post("/tickets.json").then().statusCode(201);
+
+	}
+	
+	@Test
+	public void addComment() throws Exception {
+
+		// First create a ticket we want to add a comment to
+		File jsonFile = new File("./src/main/resources/commentTicket.json");
+//		Response response = given().contentType("application/json").body(jsonFile).when().post("/tickets.json");
+//		String responseBody = response.getBody().asString();
+//		JsonPath jsonPath = new JsonPath(responseBody);
+//		int user_id = jsonPath.getInt("id");
+		
+//		System.out.println(user_id);
+		String ticketID = given().contentType("application/json").body(jsonFile).when().post("/tickets.json").then().statusCode(201)
+				.extract().path("id");
+		
+		System.out.println(ticketID);
+		
+		given().contentType("application/json").body("{\"request\": {\"comment\": {\"body\": \"Adding a new comment!\"}}}").when()
+		.put("https://bhavik.zendesk.com/api/v2/requests/"+ticketID+".json").then().statusCode(200);
+	}
+	
+	@Test
+	public void addCommentToTicker() throws Exception {
+
+		given().contentType("application/json").body("{\"request\": {\"comment\": {\"body\": \"Thanks for the information!\"}}}").when()
+		.put("https://bhavik.zendesk.com/api/v2/requests/1.json").then().statusCode(200);
+	}
+
+
+	
+
+	/*
+	 * Below test will create a new ticket by creating a new ticket object and setting
+	 * ticket parameters
+	 */
+	//@Test
+	public void createTicketSecond() throws Exception {
+
+		Tickets myTicket = new Tickets();
+		myTicket.setSubject("Creating new ticket using object model in Zendesk!");
+		myTicket.setType("task");
+		myTicket.setComment("The issue is very critical");
+		myTicket.setExternalId("1001");
+		myTicket.setPriority("urgent");
+
+		given().contentType(ContentType.JSON).body(myTicket).when().post("/tickets.json").then().statusCode(201);
 
 	}
 
